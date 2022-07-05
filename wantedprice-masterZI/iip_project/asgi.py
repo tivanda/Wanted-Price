b2c2489a65ccd1d40d6d875e3fbb7f317c44d16f
Tomislav Ivanda <60022837+tivanda@users.noreply.github.com>
2022-07-05 15:34:09 +0200

    Add files via upload

---
+++ wantedprice-masterZI/iip_project/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for iip_project project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'iip_project.settings')
+
+application = get_asgi_application()
